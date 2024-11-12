c45d9c5df6b0d6fea80b589aabfb0926ff447460
afifalhauzan <afifalhauzan2@gmail.com>
2024-11-12 20:46:02 +0700

    file baru

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./{html,js}"],
+  content: [
+    "./**/*.{html,js}",  // Scan all HTML and JS files in all subdirectories
+    "./testSoal.html",    // Scan this specific file in the root directory
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
